1a734236b8833f0c39bb4d90d5db026c64689a76
Annie <ikram.annie0@gmail.com>
2021-06-06 18:27:33 -0400

    added more calculations

--- reactcalculator/src/components/reactcalculator.js
+++ reactcalculator/src/components/reactcalculator.js
@@ -2,30 +2,45 @@ import React, { useState } from 'react'
 
 
 const Calculator = () => {
-   const [x, setX] = useState('')
-   const [operator, setOperator] = useState('')
-   const [y, setY] = useState('')
+   const [NumberOne, setNumberOne] = useState('')
+   const [NumberTwo, setNumberTwo] = useState('')
+   const [operator, setOperator] = useState('+')
    const [result, setResult] = useState('')
+   const [error, setError] = useState('')
 
    const calculate = () => { 
     setResult('')
+    setError('')
 
     switch (operator) {
         case 'add':
-            return setResult(Number(x) + Number(y))
+            return setResult(Number(NumberOne) + Number(NumberTwo))
         case 'sub':
-            return setResult(Number(x) - Number(y))
+            return setResult(Number(NumberOne) - Number(NumberTwo))
         case 'div':
-            return setResult(Number(x) / Number(y))
+            return setResult(Number(NumberOne) / Number(NumberTwo))
         case 'mul':
-            return setResult(Number(x) * Number(y))
+            return setResult(Number(NumberOne) * Number(NumberTwo))
         default:
-            return setResult('Please provide a valid number for both operands')                
+            return setError('Please provide a valid number for both operands')                
     }
 }
     return (
         <div>
-            
+            <div className="title"> React Calculator</div>
+                <input type="text" name="search" value={NumberOne} onChange={event =>
+                setNumberOne(event.target.value)} />
+                <select id="operator" value={operator} onChange={event =>
+                setOperator(event.target.value)} >
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                </select>
+                <input type="text" name="serach" value={NumberTwo} onChange={event =>
+                setNumberTwo(event.target.value)} />
+                <input type="button" value="=" onClick={calculate} />
+                <input value={result} disabled /> 
         </div>
     )
 }
