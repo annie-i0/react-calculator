2a22e935bd30a9fb397822ee15f28562bbd3c740
Annie <ikram.annie0@gmail.com>
2021-06-06 19:14:40 -0400

    css and spelling error corrections

--- reactcalculator/src/components/reactcalculator.js
+++ reactcalculator/src/components/reactcalculator.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 const Calculator = () => {
    const [NumberOne, setNumberOne] = useState('')
    const [NumberTwo, setNumberTwo] = useState('')
-   const [operator, setOperator] = useState('+')
+   const [operator, setOperator] = useState('add')
    const [result, setResult] = useState('')
    const [error, setError] = useState('')
 
@@ -27,22 +27,27 @@ const Calculator = () => {
 }
     return (
         <div>
-            <div className="title"> React Calculator</div>
+            <div className="calculator"> 
+                <h1 className="title">React Calculator</h1>
                 <input type="text" name="search" value={NumberOne} onChange={event =>
                 setNumberOne(event.target.value)} />
                 <select id="operator" value={operator} onChange={event =>
                 setOperator(event.target.value)} >
-                    <option value="+">+</option>
-                    <option value="-">-</option>
-                    <option value="/">/</option>
-                    <option value="*">*</option>
+                    <option value="add">+</option>
+                    <option value="sub">-</option>
+                    <option value="div">/</option>
+                    <option value="mul">*</option>
                 </select>
                 <input type="text" name="serach" value={NumberTwo} onChange={event =>
                 setNumberTwo(event.target.value)} />
                 <input type="button" value="=" onClick={calculate} />
-                <input value={result} disabled /> 
-        </div>
+                <input type="text" className="display" value={result} disabled /> 
+            </div>
+        </div>    
+       
+
     )
+    
 }
 
-export default Calculator;
+ export default Calculator;
